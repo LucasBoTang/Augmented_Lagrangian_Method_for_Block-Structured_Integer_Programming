4a66216a8176051a9638b012def821437c2838da
Bo Tang <botang@mie.utoronto.ca>
2024-08-22 17:25:21 +0800

    Add feasible heuristic with placeholder

---
+++ feasible_heuristic.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""
+Hueristic Algorithm to find feasible solution
+"""
+
+def sweeping(x, num_customers, num_vehicles, solution_pool):
+    """
+    sweeping technique to find a feasible solution
+    """
+    pass
+
+def packing(x, num_customers, num_vehicles, solution_pool):
+    """
+    packing technique to find a feasible solution
+    """
+    pass
